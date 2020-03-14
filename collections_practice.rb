f88f0b97da8188d300444a3adb26edc19e676d2c
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-14 20:40:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,15 @@
-def xsort_array_asc(a)
-  a.sort { |b, c| a <=> b }
-   
-  end
-  
-  
+def sort_array_asc(a)
+  a.sort {|b, c|b<=>c}
 end
 
-def sort_array_asc(a)
-   return a.to_a.sort
+def sort_array_dsc(a)
+  a.sort{|b, c|b<=>c}.reverse
+end
+
+def sort_array_char_count(a)
+  a.sort {|b.length, c.length|c.length<=>b.length}
+end
+
+def swap_elements
+  a.insert(1 a.delete.at(2))
 end
