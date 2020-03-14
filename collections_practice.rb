61e920a0356d6b99c6fa064c3c97789c95c12e4a
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-14 22:11:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,11 +48,30 @@ end
 
 
 def add_s(a)
-  
+  i = 0
+  while i < a.length
+    if i == 1
+      i++
+    else
+      a[i] << "s"
+    end
+    
 end
   
   
 
-
+def find_the_cheese(cup)        # code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  i2 = 0
+  while i2 < cheese_types.length
+    i1 = 0
+    while i1 < cup.length
+      return cup[i1] if cup[i1] == cheese_types[i2]
+      i1 += 1
+    end
+    i2 += 1
+  end
+end
 
   
