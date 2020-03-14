c0d096438dd5a014ed2774c3e942dbf3969560de
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-14 20:31:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,18 +1,6 @@
 def xsort_array_asc(a)
-  a.sort do |b, c|
-   i = 0
-   b = a[i]
-   c = a[i+1]
-    while i < a.length
-      if b > c
-        0
-      elsif b < c
-        -1
-      else
-        
-      end
-      i += 1
-    end
+  a.sort { |b, c|
+   
   end
   
   
